77c52e444ba49928d2a752474d2f4125d5d4035b
Farwacutie101 <158235713+Farwacutie101@users.noreply.github.com>
2024-03-19 17:29:49 -0400

    Unfinished

--- lab # 2.py
+++ lab # 2.py
@@ -1,1 +1,9 @@
+import pandas as pd
+
+# Create a one-dimensional array-like object using Pandas Series
+data = pd.Series([10, 20, 30, 40, 50])
+
+# Display the array-like object
+print("One-dimensional array-like object:")
+print(data)
 
